31397278a688ad0532d03181d19229ac1104230e
Parker-F <parker.f@etoncollege.org.uk>
2024-02-27 22:58:58 +0000

    Absence recorder 2 done, but date in navigation bar not changing

--- AbsenceRecorder/View/ContentView.swift
+++ AbsenceRecorder/View/ContentView.swift
@@ -9,14 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var divisions: [Division]
-
+    @State private var currentDate: Date = Date()
+    
     
     var body: some View {
-        Text("Code: \(divisions[0].code)")
-            .padding()
+        NavigationView {
+            List(divisions, id: \.self.code) { division in
+                Text("\(division.code)")
+                    .padding()
+            }
+            .navigationTitle(currentDate.getShortDate())
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { currentDate = currentDate.previousDay() }) {
+                        Image(systemName: "arrow.backward")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { currentDate = currentDate.nextDay() }) {
+                        Image(systemName: "arrow.forward")
+                    }
+                }
+            }
+        }
     }
 }
-
 #Preview {
     ContentView(divisions: Division.examples)
 }
