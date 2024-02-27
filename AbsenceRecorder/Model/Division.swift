298e87e7825bf42f4b298c3c2205c68e5e09ab06
Parker-F <parker.f@etoncollege.org.uk>
2024-02-27 18:56:27 +0000

    absence Recorder 1 done

--- AbsenceRecorder/Model/Division.swift
+++ AbsenceRecorder/Model/Division.swift
@@ -4,6 +4,10 @@
 //
 //  Created by Parker, Fred (DMT) on 27/02/2024.
 //
+let exampleForenames = ["Arianrhod", "Branwen", "Ceridwen", "Dylan", "Elen", "Ffion", "Gwydion", "Hafren", "Iolo", "Llewellyn", "Mabon", "Nia", "Owain", "Pryderi", "Rhiannon", "Seren", "Taliesin", "Urien", "Vaughan", "Wyn"]
+let exampleSurnames = ["Jones", "Williams", "Davies", "Thomas", "Jenkins", "Edwards", "Warburton", "North", "Halfpenny", "Roberts", "Charteris", "Faletau", "Tipuric", "Biggar", "Moriarty"]
+
+
 
 import Foundation
 
@@ -14,4 +18,22 @@ class Division {
     init(code: String) {
         self.code = code
     }
+    
+    #if DEBUG
+    static func createDivision(code: String, of size: Int) -> Division {
+        let division = Division(code: code)
+        
+        for i in 1...size {
+            let student = Student(forename: "\(exampleForenames[i-1])", surname: "\(exampleSurnames[i-1])", birthday: Date())
+            division.students.append(student)
+        }
+        
+        return division
+    }
+    
+    static let examples = [Division.createDivision(code: "CBioZ-2", of: 9),
+                           Division.createDivision(code: "CMsiV-2", of: 11),
+                           Division.createDivision(code: "CComW-1", of: 10),
+                           Division.createDivision(code: "CCheX-2", of: 7)]
+    #endif
 }
--- AbsenceRecorder/View/ContentView.swift
+++ AbsenceRecorder/View/ContentView.swift
@@ -8,17 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var divisions: [Division]
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Code: \(divisions[0].code)")
+            .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(divisions: Division.examples)
 }
