abc4c9a2ece18c4d4824324d4d5a287a1df45c9a
Parker-F <parker.f@etoncollege.org.uk>
2024-02-28 22:22:38 +0000

    Absence recorder 3 done: but environment stuff just isnt working, everything was working before it was added but now it isnt

---
+++ AbsenceRecorder/StateController.swift
@@ -0,0 +1,8 @@
+//
+//  StateController.swift
+//  AbsenceRecorder
+//
+//  Created by Parker, Fred (DMT) on 28/02/2024.
+//
+
+import Foundation
---
+++ AbsenceRecorder/View/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  AbsenceRecorder
+//
+//  Created by Parker, Fred (DMT) on 28/02/2024.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
